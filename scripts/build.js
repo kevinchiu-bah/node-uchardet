bd8d18a83e86ba2afdf86bc43c7f4b2dfe8a9d7b
Kevin Chiu <chiu_kevin@bah.com>
2018-05-19 01:42:10 -0400

    - Stupid other fixes

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,7 @@
 const fs = require('fs');
 const mkdir = require('mkdirp');
 const path = require('path');
-const spawn = require('cross-spawn');
+const exec = require('child_process').exec;
 const pkg = require('../package.json');
 
 function parse(args) {
@@ -51,6 +51,7 @@ function check(options) {
 
     // Rebuild on version mismatch
     if(wrapper.version() !== pkg.version) {
+      console.log(`${wrapper.version()} !== ${pkg.version}`)
       return true
     }
   } catch(e) {
@@ -66,11 +67,9 @@ function check(options) {
  */
 function build(options) {
   const cmd = [
-    '.scripts/build.sh &&',
-    process.execPath,
-    require.resolve(
-      path.join('node-gyp', 'bin', 'node-gyp.js')
-    ),
+    path.normalize('./scripts/env.sh') + ' &&',
+    'node',
+    require.resolve(path.normalize('node-gyp/bin/node-gyp.js')),
     'rebuild'
   ].concat(
   ([ 'verbose', 'debug'])
@@ -81,20 +80,20 @@ function build(options) {
       return `--${flag}`;
     })
   ).join(' ');
-
-  exec(cmd, function(error, stdout, stderr) {
-    if(!error) {
-      console.log('Binary successfully built!');
-      return;
-    } else if (errorCode === 127 ) {
-      console.error(`node-gyp isn't available at: ${process.execPath}`);
-     } else {
-       console.error('Build failed with error code:', errorCode);
-       console.info(error);
-     }
-
-     process.exit(1);
-  });
+console.log(cmd);
+  // exec(cmd, function(error, stdout, stderr) {
+  //   if(!error) {
+  //     console.log('Binary successfully built!');
+  //     return;
+  //   } else if (error.code === 127 ) {
+  //     console.error(`node-gyp isn't available at: ${gyp}`);
+  //    } else {
+  //      console.error('Build failed with error code:', error.code);
+  //      console.info(error);
+  //    }
+  //
+  //    process.exit(1);
+  // });
 }
 
 /**
